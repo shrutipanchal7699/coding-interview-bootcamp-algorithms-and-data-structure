39f04c898dfedac4b2a5a68199c0167e3961a6d7
Shruti Panchal <54422924+shrutipanchal7699@users.noreply.github.com>
2021-08-12 15:40:46 -0700

    Add files via upload

---
+++ Stack.js
@@ -0,0 +1,15 @@
+class Stack {
+    constructor() {
+        this.data = [];
+    }
+
+    push(record) {
+        this.data.push(record);
+    }
+    pop() {
+        return this.data.pop();
+    }
+    peek() {
+        return this.data[this.data.length - 1];
+    }
+}
